48073c0c32ea283e649a5f09ac93d64b55527bc3
ryan <ryanswilson@protonmail.com>
2019-09-02 22:46:57 -0400

    saving work in progress

---
+++ mediaplayer.py
@@ -0,0 +1,159 @@
+import dbus
+import dbus.service
+
+# god bless
+# https://stackoverflow.com/questions/3740903/python-dbus-how-to-export-interface-property
+
+
+class mediaPlayer(dbus.service.Object):
+    def __init__(self):
+        self.bus = dbus.SessionBus()
+        name = dbus.service.BusName("org.mpris.MediaPlayer2.pianobar", bus=self.bus)
+        super().__init__(name, "/org/mpris/MediaPlayer2")
+
+        baseProperties = {
+            "CanQuit": False,
+            "CanRaise": False,
+            "HasTrackList": False,
+            "Identity": "Please Set",
+            "SupportedUriSchemes": [],
+            "SupportedMimeTypes": [],
+        }
+        playerProperties = {
+            "PlaybackStatus": "Stopped",
+            "Rate": 1.0,
+            "Metadata": False,
+            "Volume": 1.0,
+            "Position": 1,
+            "MinimumRate": 1.0,
+            "MaximumRate": 1.0,
+            "CanGoNext": False,
+            "CanGoPrevious": False,
+            "CanPlay": False,
+            "CanPause": False,
+            "CanSeek": False,
+            "CanControl": False,
+        }
+        self.properties = {"org.mpris.MediaPlayer2": baseProperties}
+
+        basePropertiesCanWrite = {
+            "CanQuit": False,
+            "CanRaise": False,
+            "HasTrackList": False,
+            "Identity": False,
+            "SupportedUriSchemes": False,
+            "SupportedMimeTypes": False,
+        }
+        playerPropertiesCanWrite = {
+            "PlaybackStatus": False,
+            "Rate": False,
+            "Metadata": False,
+            "Volume": False,
+            "Position": False,
+            "MinimumRate": False,
+            "MaximumRate": False,
+            "CanGoNext": False,
+            "CanGoPrevious": False,
+            "CanPlay": False,
+            "CanPause": False,
+            "CanSeek": False,
+            "CanControl": False,
+        }
+        self.propertiesCanWrite = {
+            "org.mpris.MediaPlayer2": basePropertiesCanWrite,
+            "org.mpris.MediaPlayer2.Player": playerPropertiesCanWrite,
+        }
+
+        self.propertyValidator = {
+            "org.mpris.MediaPlayer2": {},
+            "org.mpris.MediaPlayer2.Player": {},
+            "org.mpris.MediaPlayer2.TrackList": {},
+            "org.mpris.MediaPlayer2.Playlists": {},
+        }
+
+    @dbus.service.method("dbus.PROPERTIES_IFACE", in_signature="ss", out_signature="v")
+    def Get(self, interfaceName, propertyName):
+        return self.GetAll(interfaceName)[propertyName]
+
+    @dbus.service.method(
+        "dbus.PROPERTIES_IFACE", in_signature="s", out_signature="a{sv}"
+    )
+    def GetAll(self, interfaceName):
+        if interfaceName in self.properties.keys():
+            return self.properties[interfaceName]
+        else:
+            raise dbus.exceptions.DBusException(
+                "org.mpris.MediaPlayer2.UnknownInterface",
+                "The Media player does not implement {0}".format(interaceName),
+            )
+
+    @dbus.service.method("dbus.PROPERTIES_IFACE", in_signature="ssv")
+    def Set(self, interfaceName, propertyName, newValue):
+        if interfaceName in self.propertiesCanWrite.keys():
+            if propertyName in self.propertiesCanWrite[interfaceName].keys():
+                canWrite = self.propertiesCanWrite[interfaceName][propertyName]
+            else:
+                # should this be an error?
+                pass
+        else:
+            raise dbus.exceptions.DBusException(
+                "org.mpris.MediaPlayer2.UnknownInterface",
+                "The Media Player Does not implement {0}".format(interfaceName),
+            )
+        if canWrite:
+            valid = self.propertyValidator[interfaceName].setdefault(
+                propertyName, lambda x: True
+            )(newValue)
+            if valid:
+                self.properties[interfaceName][propertyName] = newValue
+                self.PropertiesChanged(interfaceName, {propertyName: newValue}, [])
+
+    @dbus.service.signal("dbus.PROPERTIES_IFACE", signature="sa{sv}as")
+    def PropertiesChanged(
+        self, interfaceName, changedProperties, invalidatedProperties
+    ):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2", out_signature="")
+    def Raise(self):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2", out_signature="")
+    def Quit(self):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", out_signature="")
+    def Next(self):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", out_signature="")
+    def Previous(self):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", out_signature="")
+    def Pause(self):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", out_signature="")
+    def PlayPause(self):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", out_signature="")
+    def Stop(self):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", out_signature="")
+    def Play(self):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", in_signature="x")
+    def Seek(self, Offset):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", in_signature="ox")
+    def SetPosition(self, TrackId, Position):
+        pass
+
+    @dbus.service.method("org.mpris.MediaPlayer2.Player", in_signature="s")
+    def OpenUri(self, URI):
+        pass
